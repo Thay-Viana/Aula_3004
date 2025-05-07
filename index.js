1fb992bf270016aafbd75d6b5f936a47f3400c74
Thay Viana <thais.viana.etec@gmail.com>
2025-05-07 17:49:53 +0000

    Aula_0705

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require("express");
+const express = require('express');
 
 const app = express()
 
-app.set('view engine','hbs');
-app.set('views','./views');
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.use(express.static('public'));
 
-app.get("/", (request, response) => {
-    //response.send('Testando!!!')
-    response.render('home');
+app.get('/', (request, response) => {
+    //response.send('Testando!!!');
+    response.render('home', {nome: "Thaís"});
+});
+
+app.get('/tabuada', (request, response) => {
+    let resultado = [];
+    for(let cont = 1;cont <= 10;cont++){
+        resultado.push(cont * 5);
+    }
+    response.render('tabuada', {valores : resultado});
 });
 
 app.listen(3000, () => {
-    console.log('Servidor na porta 3000');
+    console.log('Servidor na porta  3000');
 });
